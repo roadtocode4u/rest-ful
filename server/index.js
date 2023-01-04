a84bf41a793fe590316a3d5f2c8df941330c255c
Suraj Shende <surajshende247@gmail.com>
2023-01-04 21:14:57 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URL, ()=>{
+    console.log('Connected to MongoDB');
+})
+
+// api routes starts here
+
+
+
+// api routes ends here
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
