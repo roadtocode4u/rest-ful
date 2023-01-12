d31ca308caa72e466aa9bd67fe44a26e373b7d75
Suraj Shende <surajshende247@gmail.com>
2023-01-12 20:49:22 +0530

    feat: add food item to list functionallity

--- client/src/components/FoodItemCard/FoodItemCard.js
+++ client/src/components/FoodItemCard/FoodItemCard.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './FoodItemCard.css'
 
-function FoodItemCard({category, description, imgUrl, price, title}) {
+function FoodItemCard({ category, description, imgUrl, price, title }) {
+
+  const [quantity, setQuantity] = useState(1)
+
+  async function addToList(){
+   const listObject = {
+     name: title,
+     price: price,
+     quantity: quantity
+   }
+
+   const existingList = JSON.parse(localStorage.getItem('list')) || []
+
+   existingList.push(listObject)
+
+   localStorage.setItem('list', JSON.stringify(existingList))
+  }
+
   return (
     <div className='col-md-3'>
       <div className="food-item-card">
         <div>
-          <img src={imgUrl} class="food-item-card-header-img"/>
+          <img src={imgUrl} class="food-item-card-header-img" />
         </div>
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p>{description || title}</p>
         <p>{price}/- Only</p>
         <span>{category}</span>
+
+        <div className='quantity-btn-container'>
+          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity-1)}}>-</span>
+          <span className='qnt-text'>{quantity}</span>
+          <span className='qnt-btn' onClick={(e)=>{setQuantity(quantity+1)}}>+</span>
+        </div>
+
+        <div>
+          <button type="button" className='btn-add-to-list' onClick={addToList}>Add To List</button>
+        </div>
       </div>
     </div>
   )
